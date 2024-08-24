2e05fabb3678aebe1f4e808d68215eeaaf9caa89
MohamedHesham45 <mohmedadm733+1@gmail.com>
2024-08-24 09:33:44 +0300

    edit logs

--- utils/loggerFun.js
+++ utils/loggerFun.js
@@ -1,11 +1,20 @@
 const winston = require('winston');
+const fs = require('fs');
+const path = require('path');
+
+const logDir = path.join(__dirname, '../logs');
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.File({ filename:  './logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: './logs/combined.log' }),
+    new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join(logDir, 'combined.log') }),
   ],
 });
 
